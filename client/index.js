0ec2ad36b7252666a9b83f1414ada3081901d61c
alexbaaaa <aalejandrodesarrollo@gmail.com>
2025-01-16 12:48:34 +0100

    trabajo casa

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import { io } from "./node_modules/socket.io-client/dist/socket.io.esm.min.js";
+const socket = io("http://localhost:3000");
+socket.on("connect", (data) => {
+    console.log("estoy conectado");
+    socket.on("respuesta", (dato) => {
+        console.log(dato);
+    });
+});
